fe5f225a2681837f553ec11a3f698e76829eea4c
eddie553 <eddie553@naver.com>
2021-09-05 19:03:52 +0900

    2.4.2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 function App() {
+  const name = "리액트야";
   return (
     <>
-      <h1>리액트 안녕!</h1>
+      <h1> {name} 안녕!</h1>
       <h2>잘 작동하니?</h2>
     </>
   );
