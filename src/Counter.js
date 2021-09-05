d9e31f7d94bbaca512bed4990fd65416c213f966
eddie553 <eddie553@naver.com>
2021-09-05 20:42:09 +0900

    3.4.1.3-1

--- src/Counter.js
+++ src/Counter.js
@@ -16,6 +16,7 @@ class Counter extends Component {
           onClick={() => {
             // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
             this.setState({ number: number + 1 });
+            this.setState({ number: this.state.number + 1 });
           }}
         >
           +1
