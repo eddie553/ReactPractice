e16c928c4641818c97f017ff44a0e05549c816e6
eddie553 <eddie553@naver.com>
2021-09-05 20:58:01 +0900

    3.4.1.4

--- src/Counter.js
+++ src/Counter.js
@@ -14,16 +14,10 @@ class Counter extends Component {
         <button
           // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
           onClick={() => {
-            this.setState((prevState) => {
-              return {
-                number: prevState.number + 1,
-              };
+            this.setState({ number: number + 1 }, () => {
+              console.log("방금 setState가 호출되었습니다.");
+              console.log(this.state);
             });
-            // 위 코드와 아래 코드는 완전히 똑같은 기능을 합니다.
-            // 아래 코드는 함수에서 바로 객체를 반환한다는 의미입니다.
-            this.setState((prevState) => ({
-              number: prevState.number + 1,
-            }));
           }}
         >
           +1
