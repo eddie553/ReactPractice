3d61c0ddecc87e3ae0257fc1bb465b16acc01cf2
eddie553 <eddie553@naver.com>
2021-09-05 19:10:35 +0900

    2.4.5-3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function App() {
   const name = undefined;
-  return name || "값이 undefined입니다";
+  return <div>{name}</div>; //no error, undefined render
 }
 
 export default App;
