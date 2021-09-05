4e7ec37fbda8b29da11db9c744f8011c2e63ccf3
eddie553 <eddie553@naver.com>
2021-09-05 20:10:46 +0900

    3.3.4-1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React from "react";
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent />;
+  return <MyComponent>리액트</MyComponent> />;
 };
 export default App;
