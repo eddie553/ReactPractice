72d4732cdf6710391aca9ed92f232e0c62c3cd9c
eddie553 <eddie553@naver.com>
2021-09-05 19:43:46 +0900

    3.1-1

--- src/App.js
+++ src/App.js
@@ -3,18 +3,7 @@ import "./App.css";
 
 function App() {
   const name = "리액트";
-  return (
-    <>
-      {/* 주석 작성하는 법 */}
-      <div
-        className="react" //시작 태그를 여러줄로 작성하게 된다면 여기에 주석 가능
-      >
-        {name}
-      </div>
-      //이런 주석은 그대로
-      <input />
-    </>
-  );
+  return <div className="react">{name}</div>;
 }
 
 export default App;
