d52b538b5dabf1fb3b0d2897007233062ab33508
eddie553 <eddie553@naver.com>
2021-09-05 21:20:24 +0900

    4.2.2.1-1

--- src/EventPractive.js
+++ src/EventPractive.js
@@ -5,6 +5,14 @@ class EventPractice extends Component {
     return (
       <div>
         <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력하세요"
+          onChange={(e) => {
+            console.log(e);
+          }}
+        />
       </div>
     );
   }
