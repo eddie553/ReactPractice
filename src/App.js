883343a7d67848c91adae23e4c86796a39c75918
eddie553 <eddie553@naver.com>
2021-09-05 18:56:06 +0900

    2.4.1-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg'; //import modules
-import './App.css'; //import css file 
+import logo from './logo.svg'; 
+import './App.css'; 
 
-function App() { //make component
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ <h1>리액트 안녕!</h1> 
+ <h2>잘 작동하니?</h2>
+  )
 }
 
 export default App;
