d84e6fade7e581b47cd3b426d4b81095b2c75d63
eddie553 <eddie553@naver.com>
2021-09-05 19:43:57 +0900

    3.1-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  const name = "리액트";
-  return <div className="react">{name}</div>;
+class App extends Component {
+  render() {
+    const name = "react";
+    return <div className="react">{name}</div>;
+  }
 }
 
 export default App;
