e684e9b6d9b4cef6b022f6018bfd9c0e53090ffd
eddie553 <eddie553@naver.com>
2021-09-05 19:54:07 +0900

    3.3.3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React from "react";
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent name="React" />;
+  return <MyComponent />;
 };
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -3,4 +3,7 @@ import React from "react";
 const MyComponent = (props) => {
   return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>;
 };
+MyComponent.defaultProps = {
+  name: "기본이름",
+};
 export default MyComponent;
