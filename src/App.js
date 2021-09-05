f0f7a8e594e7b7de6a2bb7f2bdf06ccde06e6025
eddie553 <eddie553@naver.com>
2021-09-05 18:58:46 +0900

    2.4.1-4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 function App() {
   return (
-    <Fragment>
+    <>
       <h1>리액트 안녕!</h1>
       <h2>잘 작동하니?</h2>
-    </Fragment>
+    </>
   );
 }
 
