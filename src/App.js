e353cc6dcd484072cefb99ebdb5bf84aaed83982
eddie553 <eddie553@naver.com>
2021-09-05 19:08:13 +0900

    2.4.4-2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 function App() {
-  const name = "리웩트";
-  return <div>{name === "리액트" ? <h1> 리액트입니다.</h1> : null}</div>;
+  const name = "리액트";
+  return <div>{name === "리액트" && <h1> 리액트입니다.</h1>}</div>;
 }
 
 export default App;
