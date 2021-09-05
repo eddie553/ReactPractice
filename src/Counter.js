42b706952f04939568192ae5b6ddbc23708f1400
eddie553 <eddie553@naver.com>
2021-09-05 20:40:51 +0900

    3.4.1.2

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    // state의 초깃값 설정하기
-    this.state = {
-      number: 0,
-      fixedNumber: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
   render() {
     const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회합니다.
     return (
