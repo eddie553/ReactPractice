399f60cb95eaeb7a81ba34e7738fb5db904f46c9
eddie553 <eddie553@naver.com>
2021-09-10 16:20:28 +0900

    4.2.4

--- src/EventPractive.js
+++ src/EventPractive.js
@@ -2,19 +2,21 @@ import React, { Component } from "react";
 
 class EventPractice extends Component {
   state = {
+    username: "",
     message: "",
   };
 
   handleChange = (e) => {
     this.setState({
-      message: e.target.value,
+      [e.target.name]: e.target.value,
     });
   };
 
   handleClick = () => {
-    alert(this.state.message);
+    alert(this.state.username + ":" + this.state.message);
     this.setState({
-      message: " ",
+      username: "",
+      message: "",
     });
   };
 
@@ -24,6 +26,14 @@ class EventPractice extends Component {
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="
+      text"
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
