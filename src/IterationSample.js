a73f0938d522cc939f8717327d7ec53f2bbb0b3d
eddie553 <eddie553@naver.com>
2021-09-10 21:21:48 +0900

    6.4.2-2

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -11,12 +11,21 @@ const IterationSample = () => {
   const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
 
   const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
 
   const namesList = names.map((name) => <li key={name.id}>{name.text}</li>);
   return (
     <>
       <input value={inputText} onChange={onChange} />
-      <button>추가</button>
+      <button onClick={onClick}>추가</button>
       <ul>{namesList}</ul>
     </>
   );
