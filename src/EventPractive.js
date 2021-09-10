f3fb46aa9eb1b7bb6bb7450d5932a4800a36d039
eddie553 <eddie553@naver.com>
2021-09-10 16:13:13 +0900

    4.2.3.2

--- src/EventPractive.js
+++ src/EventPractive.js
@@ -2,27 +2,21 @@ import React, { Component } from "react";
 
 class EventPractice extends Component {
   state = {
-    message: " ",
+    message: "",
   };
 
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({
       message: e.target.value,
     });
-  }
+  };
 
-  handleClick() {
+  handleClick = () => {
     alert(this.state.message);
     this.setState({
       message: " ",
     });
-  }
+  };
 
   render() {
     return (
