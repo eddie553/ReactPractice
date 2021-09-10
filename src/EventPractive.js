3b21467fe9015426bc880eb58065c8690319b20c
eddie553 <eddie553@naver.com>
2021-09-10 16:29:08 +0900

    4.3-2

--- src/EventPractive.js
+++ src/EventPractive.js
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
 
 const EventPractice = () => {
-  const [username, setUsername] = useState("");
-  const [message, setMessage] = useState("");
-  const onChangeUsername = (e) => setUsername(e.target.value);
-  const onChangeMessage = (e) => setMessage(e.target.value);
+  const [form, setForm] = useState({
+    username: "",
+    message: "",
+  });
+  const { username, message } = form;
+  const onChange = (e) => {
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value,
+    };
+    setForm(nextForm);
+  };
   const onClick = () => {
     alert(username + ": " + message);
-    setUsername("");
-    setMessage("");
+    setForm({
+      username: "",
+      message: "",
+    });
   };
   const onKeyPress = (e) => {
     if (e.key === "Enter") {
@@ -19,21 +29,18 @@ const EventPractice = () => {
     <div>
       <h1>이벤트 연습</h1>
       <input
-        type="text
-        "
+        type="text"
         name="username"
-        placeholder="사용자명
-        "
+        placeholder="사용자명"
         value={username}
-        onChange={onChangeUsername}
+        onChange={onChange}
       />
       <input
         type="text"
         name="message"
-        placeholder="아무거나 입력해 보세요
-        "
+        placeholder="아무거나 입력해 보세요"
         value={message}
-        onChange={onChangeMessage}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
