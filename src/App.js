ceb04a5b61b928d94ac3c27f3f00f934a493e338
eddie553 <eddie553@naver.com>
2021-09-12 22:02:52 +0900

    8.1.1-2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
-import Counter from "./Counter";
+import Info from "./Info";
 
 const App = () => {
-  return <Counter />;
+  return <Info />;
 };
 export default App;
