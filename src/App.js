620929a914a83d8bc358abafb78a918267effe76
eddie553 <eddie553@naver.com>
2021-09-12 22:10:00 +0900

    8.2.1-2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import Info from "./Info";
 
 const App = () => {
--- src/Info.js
+++ src/Info.js
@@ -4,12 +4,8 @@ const Info = () => {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
   useEffect(() => {
-    console.log("렌더링이 완료되었습니다");
-    console.log({
-      name,
-      nickname,
-    });
-  });
+    console.log("마운트될 때만 실행됩니다");
+  }, []);
 
   const onChangeName = (e) => {
     setName(e.target.value);
