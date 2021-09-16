5797e4edba81f9691b31d17c732627043d984955
eddie553 <eddie553@naver.com>
2021-09-16 23:51:30 +0900

    10.2.2-1

---
+++ src/TodoInsert.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { MdAdd } from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert = () => {
+  return (
+    <form className="TodoInsert">
+      <input placeholder="할 일을 입력해라" />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+export default TodoInsert;
