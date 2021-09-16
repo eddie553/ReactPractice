8caa2833d25e549cbb0f2719fe2bd42abd593597
eddie553 <eddie553@naver.com>
2021-09-16 23:23:09 +0900

    10.2.1-2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TodoTemplate from './TodoTemplate';
 
 const App = () => {
-  return <div>Todo App을 만들자!</div>;
+  return <TodoTemplate> Todo App을 만들자! </TodoTemplate>;
 };
 
 export default App;
