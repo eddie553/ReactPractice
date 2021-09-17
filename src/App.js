ba0943f757e638d07ead7e4788a559b1de7e590a
eddie553 <eddie553@naver.com>
2021-09-17 19:24:11 +0900

    11.1

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
 const App = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, text: '리액트의 기초 알아보기', checked: true },
-    { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
-    { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
-  ]);
-
+  const [todos, setTodos] = useState(createBulkTodos);
   const nextId = useRef(4);
   const onInsert = useCallback(
     (text) => {
