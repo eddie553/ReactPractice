e795881a1c1a7ce5e215ad9b1f63fb01e3e18ae4
eddie553 <eddie553@naver.com>
2021-09-17 20:04:55 +0900

    12.1.5

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     (e) => {
       const { name, value } = e.target;
       setForm(
-        produce(form, (draft) => {
+        produce((draft) => {
           draft[name] = value;
         })
       );
@@ -32,7 +32,7 @@ const App = () => {
         username: form.username,
       };
       setData(
-        produce(data, (draft) => {
+        produce((draft) => {
           draft.array.push(info);
         })
       );
@@ -50,7 +50,7 @@ const App = () => {
   const onRemove = useCallback(
     (id) => {
       setData(
-        produce(data, (draft) => {
+        produce((draft) => {
           draft.array.splice(
             draft.array.findIndex((info) => info === id),
             1
