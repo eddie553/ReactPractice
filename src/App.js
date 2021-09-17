28b364b9e6281c08495593ba9cc9da1258c1703b
eddie553 <eddie553@naver.com>
2021-09-18 01:54:35 +0900

    13.2.5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
     </div>
