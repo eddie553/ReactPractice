732fadfcb55ecaa44226489c0b41b1e9e49bd2ce
eddie553 <eddie553@naver.com>
2021-09-18 02:04:07 +0900

    13.5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Profile from "./Profile";
+import Profiles from "./Profiles";
 
 const App = () => {
   return (
@@ -15,16 +16,14 @@ const App = () => {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/gildong">gildong 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact />
       <Route path={["/about", "/info"]} component={About} />
       <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 };
