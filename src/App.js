6321112bb3eea360fa5018c65ce2728514d5875a
eddie553 <eddie553@naver.com>
2021-09-17 18:57:55 +0900

    10.3.2.3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState, useRef } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -9,9 +9,23 @@ const App = () => {
     { id: 3, text: '컴포넌트 스타일링해 보기', checked: true },
     { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
   ]);
+
+  const nextId = useRef(4);
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
   return (
     <TodoTemplate>
-      <TodoInsert /> <TodoList todos={todos} />
+      <TodoInsert onInsert={onInsert} /> <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
