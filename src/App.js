22a90043ddea6882833e4efc49bfc878d730a035
eddie553 <eddie553@naver.com>
2021-09-18 02:08:28 +0900

    13.6.1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./About";
 import Home from "./Home";
 import Profile from "./Profile";
 import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 const App = () => {
   return (
@@ -18,12 +19,16 @@ const App = () => {
         <li>
           <Link to="/profiles">프로필</Link>
         </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact />
       <Route path={["/about", "/info"]} component={About} />
       <Route path="/profile/:username" component={Profile} />
       <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 };
