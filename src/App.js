3964611cfbe8ef2e84317635b67d402c3d489c7b
eddie553 <eddie553@naver.com>
2021-09-17 19:30:11 +0900

    11.5.1

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
         text,
         checked: false,
       };
-      setTodos(todos.concat(todo));
+      setTodos((todos) => todos.concat(todo));
       nextId.current += 1;
     },
     [todos],
@@ -32,14 +32,14 @@ const App = () => {
 
   const onRemove = useCallback(
     (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
+      setTodos((todos) => todos.filter((todo) => todo.id !== id));
     },
     [todos],
   );
 
   const onToggle = useCallback(
     (id) => {
-      setTodos(
+      setTodos((todos) =>
         todos.map((todo) =>
           todo.id === id ? { ...todo, checked: !todo.checked } : todo,
         ),
