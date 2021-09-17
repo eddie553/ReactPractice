d81125beec4b0bfe92dcfe5281809fb365e560cc
eddie553 <eddie553@naver.com>
2021-09-17 19:05:33 +0900

    10.3.3.2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TodoList from './components/TodoList';
 const App = () => {
   const [todos, setTodos] = useState([
     { id: 1, text: '리액트의 기초 알아보기', checked: true },
-    { id: 3, text: '컴포넌트 스타일링해 보기', checked: true },
+    { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
     { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
   ]);
 
@@ -23,9 +23,18 @@ const App = () => {
     },
     [todos],
   );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert onInsert={onInsert} /> <TodoList todos={todos} />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} />
     </TodoTemplate>
   );
 };
