416916a48e66a72484a56bbe6646595ddace25a4
eddie553 <eddie553@naver.com>
2021-09-19 22:37:01 +0900

    15.3.2-2

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -4,20 +4,20 @@ import { ColorConsumer } from "../contexts/color";
 const ColorBox = () => {
   return (
     <ColorConsumer>
-      {(value) => (
+      {({ state }) => (
         <>
           <div
             style={{
               width: "64px",
               height: "64px",
-              background: value.state.color,
+              background: state.color,
             }}
           />
           <div
             style={{
               width: "32px",
               height: "32px",
-              background: value.state.subcolor,
+              background: state.subcolor,
             }}
           />
         </>
