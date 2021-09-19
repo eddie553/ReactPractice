66cd577957aa07bd9624a62c08efcd173713c32c
eddie553 <eddie553@naver.com>
2021-09-19 21:42:08 +0900

    14.2-2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => {
-        setData(response.data);
-      });
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
     <div>
