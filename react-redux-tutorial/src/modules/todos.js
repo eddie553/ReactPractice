1623ef709286642c7dd8b483e6e8323a9ac419b2
eddie553 <eddie553@naver.com>
2021-09-25 18:56:27 +0900

    17.3.2.1

---
+++ react-redux-tutorial/src/modules/todos.js
@@ -0,0 +1,4 @@
+const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
+const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
+const TOGGLE = 'todos/TOGGLE'; // todo를 체크/체크 해제함
+const REMOVE = 'todos/REMOVE'; // todo를 제거함
