19f804002b463d4ffbf18bc9ed8c3c7100ba2c24
eddie553 <eddie553@naver.com>
2021-09-25 20:47:26 +0900

    17.7.2-2

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -6,12 +6,11 @@ import { increase, decrease } from '../modules/counter';
 const CounterContainer = () => {
   const number = useSelector((state) => state.counter.number);
   const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
   return (
-    <Counter
-      number={number}
-      onIncrease={() => dispatch(increase())}
-      onDecrease={() => dispatch(decrease())}
-    />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
