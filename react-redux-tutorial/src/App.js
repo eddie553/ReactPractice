321079c4a3b857253c618088a6c20e28984173db
eddie553 <eddie553@naver.com>
2021-09-25 20:15:30 +0900

    17.5.2

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import CounterContainer from './containers/CounterContainer';
-import Todos from './components/Todos';
+import TodosContainer from './containers/TodosContainer';
 
 const App = () => {
   return (
     <div>
       <CounterContainer />
       <hr />
-      <Todos />
+      <TodosContainer />
     </div>
   );
 };
