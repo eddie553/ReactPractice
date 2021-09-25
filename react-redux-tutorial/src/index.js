3a1c5e25d914b70ed8d0efec7ed4ebba42735a65
eddie553 <eddie553@naver.com>
2021-09-25 19:08:58 +0900

    17.4.2

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-//import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import rootReducer from './modules';
@@ -9,8 +9,8 @@ import rootReducer from './modules';
 const store = createStore(rootReducer);
 
 ReactDOM.render(
-  //<Provider store={store}>
-  <App />,
-  //</Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
