3d8299d2b2e20323bfeeab22db2a900cd0269dc3
eddie553 <eddie553@naver.com>
2021-09-25 20:02:19 +0900

    17.5.1-4

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
 
 const CounterContainer = ({ number, increase, decrease }) => {
   return (
@@ -12,12 +13,11 @@ const mapStateToProps = (state) => ({
   number: state.counter.number,
 });
 const mapDispatchToProps = (dispatch) => ({
-  // 임시 함수
   increase: () => {
-    console.log('increase');
+    dispatch(increase());
   },
   decrease: () => {
-    console.log('decrease');
+    dispatch(decrease());
   },
 });
 export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
