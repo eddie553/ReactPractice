e017179c066f6f601a8bca850db4953c2d7f7be8
eddie553 <eddie553@naver.com>
2021-09-25 20:54:37 +0900

    17.7.5

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,21 +1,19 @@
-import React, { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
+import useActions from '../lib/useActions';
 
 const TodosContainer = () => {
   const { input, todos } = useSelector(({ todos }) => ({
     input: todos.input,
     todos: todos.todos,
   }));
-  const dispatch = useDispatch();
-  const onChangeInput = useCallback(
-    (input) => dispatch(changeInput(input)),
-    [dispatch],
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    [],
   );
-  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
-  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
-  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
 
   return (
     <Todos
