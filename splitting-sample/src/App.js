8a3fac5fb822a6a6e98b7230be68bdb57cfb7f3d
eddie553 <eddie553@naver.com>
2021-09-26 20:04:55 +0900

    19.1-2

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import notify from "./notify";
 
 function App() {
   const onClick = () => {
-    notify();
+    import("./notify").then((result) => result.default());
   };
   return (
     <div className="App">
