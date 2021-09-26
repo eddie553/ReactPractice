e412532d8d98b67342a46a6250ca444218bf9463
eddie553 <eddie553@naver.com>
2021-09-26 20:21:12 +0900

    19.2.3-2

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import loadable from "@loadable/component";
-
 const SplitMe = loadable(() => import("./SplitMe"), {
   fallback: <div>loading...</div>,
 });
@@ -12,11 +11,16 @@ function App() {
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
         {visible && <SplitMe />}
       </header>
     </div>
