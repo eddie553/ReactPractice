141e9d9ca819935797788261c7461767af8d6017
eddie553 <eddie553@naver.com>
2021-09-26 19:35:18 +0900

    18.3.2.4

--- learn-redux-middleware/src/lib/createRequestThunk.js
+++ learn-redux-middleware/src/lib/createRequestThunk.js
@@ -1,29 +1,25 @@
-import { finishLoading, startLoading } from "../modules/loading";
+import { call, put } from "redux-saga/effects";
+import { startLoading, finishLoading } from "../modules/loading";
 
-export default function createRequestThunk(type, request) {
-  // 성공 및 실패 액션 타입을 정의합니다.
+export default function createRequestSaga(type, request) {
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
-  return (params) => async (dispatch) => {
-    dispatch({ type }); // 시작됨
-    dispatch(startLoading(type));
+
+  return function* (action) {
+    yield put(startLoading(type)); // 로딩 시작
     try {
-      const response = await request(params);
-      dispatch({
+      const response = yield call(request, action.payload);
+      yield put({
         type: SUCCESS,
         payload: response.data,
-      }); // 성공
-      dispatch(finishLoading(type));
+      });
     } catch (e) {
-      dispatch({
+      yield put({
         type: FAILURE,
         payload: e,
         error: true,
-      }); // 에러 발생
-      dispatch(startLoading(type));
-      throw e;
+      });
     }
+    yield put(finishLoading(type)); // 로딩 끝
   };
 }
-
-// 사용법: createRequestThunk('GET_USERS',api.getUsers);
