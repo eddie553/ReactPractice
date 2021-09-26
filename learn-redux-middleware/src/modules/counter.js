8412409522a42b5bdfa44573fd75e5a4984ef4d3
eddie553 <eddie553@naver.com>
2021-09-26 19:39:17 +0900

    18.3.2.5-2

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,5 +1,12 @@
 import { createAction, handleActions } from "redux-actions";
-import { delay, put, takeEvery, takeLatest, select } from "redux-saga/effects";
+import {
+  delay,
+  put,
+  takeEvery,
+  takeLatest,
+  select,
+  throttle,
+} from "redux-saga/effects";
 
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
@@ -27,7 +34,7 @@ function* decreaseSaga() {
 
 export function* counterSaga() {
   // takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리해 줍니다.
-  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  yield throttle(3000, INCREASE_ASYNC, increaseSaga);
   // takeLatest는 기존에 진행 중이던 작업이 있다면 취소 처리하고
   // 가장 마지막으로 실행된 작업만 수행합니다.
   yield takeLatest(DECREASE_ASYNC, decreaseSaga);
