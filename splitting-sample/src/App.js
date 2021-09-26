56bd0f07688c51b1229ee0967bf77c7071a29ce2
eddie553 <eddie553@naver.com>
2021-09-26 20:12:16 +0900

    19.2.2

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,29 +1,25 @@
-import React, { Component } from "react";
+import React, { useState, Suspense } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  state = {
-    SplitMe: null,
+const SplitMe = React.lazy(() => import("./SplitMe"));
+
+function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
   };
-  handleClick = async () => {
-    const loadedModule = await import("./SplitMe");
-    this.setState({
-      SplitMe: loadedModule.default,
-    });
-  };
-  render() {
-    const { SplitMe } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handleClick}>Hello React!</p>
-          {SplitMe && <SplitMe />}
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick}>Hello React!</p>
+        <Suspense fallback={<div>loading…</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
+      </header>
+    </div>
+  );
 }
 
 export default App;
