14a9e3327e542dde99182faf80a9ee8404c00233
eddie553 <eddie553@naver.com>
2021-09-30 01:36:13 +0900

    20.4.4-1

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -5,6 +5,10 @@ import { StaticRouter } from "react-router-dom";
 import App from "./App";
 import path from "path";
 import fs from "fs";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./modules";
 
 // asset-manifest.json에서 파일 경로들을 조회합니다.
 const manifest = JSON.parse(
@@ -50,11 +54,14 @@ const serverRender = async (req, res, next) => {
   // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버사이드 렌더링을 해줍니다.
 
   const context = {};
+  const store = createStore(rootReducer, applyMiddleware(thunk));
 
   const jsx = (
-    <StaticRouter location={req.url} context={context}>
-      <App />
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={context}>
+        <App />
+      </StaticRouter>
+    </Provider>
   );
 
   const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
