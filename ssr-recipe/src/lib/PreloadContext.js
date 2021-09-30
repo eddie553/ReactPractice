694b4035b872406b4db1f2089fcb14b053abf7f6
eddie553 <eddie553@naver.com>
2021-09-30 23:23:19 +0900

    20.4.9

--- ssr-recipe/src/lib/PreloadContext.js
+++ ssr-recipe/src/lib/PreloadContext.js
@@ -17,3 +17,15 @@ export const Preloader = ({ resolve }) => {
   preloadContext.promises.push(Promise.resolve(resolve()));
   return null;
 };
+
+// Hook 형태로 사용 할 수 있는 함수
+export const usePreloader = (resolve) => {
+  const preloadContext = useContext(PreloadContext);
+  if (!preloadContext) return null;
+  if (preloadContext.done) return null;
+
+  // promises 배열에 프로미스 등록
+  // 설령 resolve 함수가 프로미스를 반환하지 않더라도, 프로미스 취급을 하기 위하여
+  // Promise.resolve 함수 사용
+  preloadContext.promises.push(Promise.resolve(resolve()));
+};
