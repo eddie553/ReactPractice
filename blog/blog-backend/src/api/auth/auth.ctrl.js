31524123272ecc3ba0238caa449ae769536ee473
eddie553 <eddie553@naver.com>
2021-10-03 01:48:46 +0900

    23.4.2

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -38,6 +38,10 @@ export const register = async (ctx) => {
 
     //응답할 데이터에서 hashedPassword 필드 제거
     ctx.body = user.serialize();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, //7days
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -66,6 +70,12 @@ export const login = async (ctx) => {
       return;
     }
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, //7days
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
