dd2cec7d9c0bc5a6ba2d20cfd4f58d65a6c96eca
eddie553 <eddie553@naver.com>
2021-10-03 01:54:54 +0900

    23.4.3-2

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -81,9 +81,19 @@ export const login = async (ctx) => {
   }
 };
 
+/*
+  GET /api/auth/check
+*/
 export const check = async (ctx) => {
-  // 로그인 상태 확인
+  const { user } = ctx.state;
+  if (!user) {
+    // 로그인 중 아님
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+  ctx.body = user;
 };
+
 export const logout = async (ctx) => {
   // 로그아웃
 };
