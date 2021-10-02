b0dfe6d1e5c1db6df92163c6874c08babba9b3ae
eddie553 <eddie553@naver.com>
2021-10-02 18:15:22 +0900

    22.4

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,38 +1,5 @@
-require('dotenv').config();
+// 이 파일에서만 no-global-assign ESLint 옵션을 비활성화합니다.
+/* eslint-disable no-global-assign */
 
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-const mongoose = require('mongoose');
-
-// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
-const { PORT, MONGO_URI } = process.env;
-
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((e) => {
-    console.error(e);
-  });
-
-const api = require('./api');
-
-const app = new Koa();
-const router = new Router();
-
-// 라우터 설정
-router.use('/api', api.routes()); // api 라우트 적용
-
-// 라우터 적용 전에 bodyParser 적용
-app.use(bodyParser());
-
-// app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods());
-
-// PORT가 지정되어 있지 않다면 4000을 사용
-const port = PORT || 4000;
-app.listen(port, () => {
-  console.log('Listening to port %d', port);
-});
+require = require('esm')(module /*, options*/);
+module.exports = require('./main.js');
