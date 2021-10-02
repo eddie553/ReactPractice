4bd9dd42740bb5aa8e15db4f97551c7ee1efcacf
eddie553 <eddie553@naver.com>
2021-10-03 00:51:57 +0900

    22.10.6-1

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -60,7 +60,13 @@ export const list = async (ctx) => {
       .exec();
     const postCount = await Post.countDocuments().exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10));
-    ctx.body = posts;
+    ctx.body = posts
+      .map((post) => post.toJSON())
+      .map((post) => ({
+        ...post,
+        body:
+          post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+      }));
   } catch (e) {
     ctx.throw(500, e);
   }
