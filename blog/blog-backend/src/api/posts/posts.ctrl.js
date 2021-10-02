d954308762193611a0710179f3c9aa69fe46ea3f
eddie553 <eddie553@naver.com>
2021-10-03 00:41:50 +0900

    22.10.4

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -47,8 +47,17 @@ export const write = async (ctx) => {
   GET /api/posts
 */
 export const list = async (ctx) => {
+  const page = parseInt(ctx.query.page || '1', 10);
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
   try {
-    const posts = await Post.find().sort({ _id: -1 }).limit(10).exec();
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .exec();
     ctx.body = posts;
   } catch (e) {
     ctx.throw(500, e);
