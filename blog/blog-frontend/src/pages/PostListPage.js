bb849777534f565e7bcdb00a2ab8cca3ccb21d1c
eddie553 <eddie553@naver.com>
2021-10-03 19:54:24 +0900

    26.2.2

--- blog/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-frontend/src/pages/PostListPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import HeaderContainer from '../containers/common/HeaderContainer';
 import PostList from '../components/posts/PostList';
+import HeaderContainer from '../containers/common/HeaderContainer';
+import PostListContainer from '../containers/posts/PostListContainer';
 
 const PostListPage = () => {
   return (
     <>
       <HeaderContainer />
-      <PostList />
+      <PostListContainer />
     </>
   );
 };
