88555bcffb011a6b0133c7b17b8a54ebcc287a20
eddie553 <eddie553@naver.com>
2021-10-03 19:43:41 +0900

    26.2.1-2

--- blog/blog-frontend/src/components/post/PostViewer.js
+++ blog/blog-frontend/src/components/post/PostViewer.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
-// import SubInfo from '../common/SubInfo';
-// import Tags from '../common/Tags';
+import SubInfo from '../common/SubInfo';
+import Tags from '../common/Tags';
 
 const PostViewerBlock = styled(Responsive)`
   margin-top: 4rem;
@@ -16,32 +16,6 @@ const PostHead = styled.div`
     font-size: 3rem;
     line-height: 1.5;
     margin: 0;
-  }
-`;
-
-const SubInfo = styled.div`
-  margin-top: 1rem;
-  color: ${palette.gray[6]};
-
-  /* span 사이에 가운데점 문자 보여 주기 */
-  span + span::before {
-    color: ${palette.gray[5]};
-    padding-left: 0.25rem;
-    padding-right: 0.25rem;
-    content: '\\B7'; /* 가운데점 문자 */
-  }
-`;
-
-const Tags = styled.div`
-  margin-top: 0.5rem;
-  .tag {
-    display: inline-block;
-    color: ${palette.cyan[7]};
-    text-decoration: none;
-    margin-right: 0.5rem;
-    &:hover {
-      color: ${palette.cyan[6]};
-    }
   }
 `;
 
@@ -59,7 +33,7 @@ const PostViewer = ({ post, error, loading }) => {
     return <PostViewerBlock>오류 발생!</PostViewerBlock>;
   }
 
-  // 로딩 중이거나 아직 포스트 데이터가 없을 때
+  // 로딩중이거나, 아직 포스트 데이터가 없을 시
   if (loading || !post) {
     return null;
   }
@@ -69,17 +43,12 @@ const PostViewer = ({ post, error, loading }) => {
     <PostViewerBlock>
       <PostHead>
         <h1>{title}</h1>
-        <SubInfo>
-          <span>
-            <b>{user.username}</b>
-          </span>
-          <span>{new Date(publishedDate).toLocaleDateString()}</span>
-        </SubInfo>
-        <Tags>
-          {tags.map((tag) => (
-            <div className="tag">#{tag}</div>
-          ))}
-        </Tags>
+        <SubInfo
+          username={user.username}
+          publishedDate={publishedDate}
+          hasMarginTop
+        />
+        <Tags tags={tags} />
       </PostHead>
       <PostContent dangerouslySetInnerHTML={{ __html: body }} />
     </PostViewerBlock>
