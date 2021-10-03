c9acff5215e45d48192daaddb237f4ea68787da6
eddie553 <eddie553@naver.com>
2021-10-03 14:08:04 +0900

    24.2.1

---
+++ blog/blog-frontend/src/components/base/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/**
+ * 회원가입/로그인 페이지의 레이아웃을 담당하는 컴포넌트입니다.
+ */
+
+const HeaderBlock = styled.div``;
+
+const Header = () => {
+  return <HeaderBlock></HeaderBlock>;
+};
+
+export default Header;
