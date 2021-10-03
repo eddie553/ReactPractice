7a8986ef933e3f4ed7f520b103e990e39315c88f
eddie553 <eddie553@naver.com>
2021-10-03 15:43:47 +0900

    24.2.4-3

--- blog/blog-frontend/src/containers/auth/RegisterForm.js
+++ blog/blog-frontend/src/containers/auth/RegisterForm.js
@@ -3,8 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
 
-const RegisterForm = () => {
+const RegisterForm = ({ history }) => {
   const dispatch = useDispatch();
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
@@ -57,10 +58,9 @@ const RegisterForm = () => {
   // user 값이 잘 설정되었는지 확인
   useEffect(() => {
     if (user) {
-      console.log('check API 성공');
-      console.log(user);
+      history.push('/'); // 홈 화면으로 이동
     }
-  }, [user]);
+  }, [history, user]);
 
   return (
     <AuthForm
@@ -72,4 +72,4 @@ const RegisterForm = () => {
   );
 };
 
-export default RegisterForm;
+export default withRouter(RegisterForm);
