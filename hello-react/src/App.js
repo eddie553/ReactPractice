2368c70e6b0ad36ba1bc2c828ea869d035ff6701
eddie553 <eddie553@naver.com>
2021-11-23 21:35:10 +0900

    2.4.8-1

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,17 @@
-// 2.4.7 class 대신 className
+// 2.4.8 태그는 꼭 닫아주기
+// 태그를 닫지 않아 생기는 오류 코드
 
 import React from "react";
 import "./App.css";
 
 function App() {
   const name = "리액트";
-  return <div className="react">{name}</div>;
+  return (
+  <> 
+    <div className="react">{name}</div>;
+    <input> 
+  </>
+  );
 }
 
 export default App;
