a88f394aa0d16984ffe89a1cdb9e793b1334c770
eddie553 <eddie553@naver.com>
2021-11-23 21:27:44 +0900

    2.4.5-2

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,11 @@
 // 2.4.5 undefined 반환시 오류
+// return 반환 시에 OR 연산자로 undefined 반환 방지하기
 
 import React from "react";
 
 function App() {
   const name = undefined;
-  return name;
+  return name || "값이 undefined입니다";
 }
 
 export default App;
