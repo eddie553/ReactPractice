06203841234f4b896cfe1b80334c431d499769f8
eddie553 <eddie553@naver.com>
2021-11-23 23:53:02 +0900

    3.3.6.1

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,13 @@
-// 3.3 propTypes를 통한 props 검증
-// propTypes를 string으로 설정하고, string 타입인 props를 전달하여 오류 해결
+// 3.3 isRequired로 필수 props 설정하기
 
 import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent name="react">리액트</MyComponent>;
+  return (
+    <MyComponent name="react" favoriteNumber={1}>
+      리액트
+    </MyComponent>
+  );
 };
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,23 +1,27 @@
-// 3.3 propTypes를 통한 props 검증
+// 3.3 isRequired로 필수 props 설정하기
 
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, children }) => {
-  // 비구조화 할당
+const MyComponent = ({ name, favoriteNumber, children }) => {
   return (
     <div>
       안녕하세요, 제 이름은 {name}입니다.
       <br />
       children 값은 {children}
       입니다.
+      <br />
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.
     </div>
   );
 };
 MyComponent.defaultProps = {
   name: '기본이름',
 };
+
 MyComponent.propTypes = {
   name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
 };
+
 export default MyComponent;
