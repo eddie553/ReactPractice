7acb74f691a334eb5ede6ddac2b41a619d98e0bd
eddie553 <eddie553@naver.com>
2021-11-23 21:24:41 +0900

    2.4.4-1

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,18 +1,11 @@
+// 2.4.4 && 연산자를 사용한 조건부 렌더링
+// 조건을 만족하지 않을 때 아무것도 렌더링하지않음
+
 import React from "react";
 
-// 2.4.3 삼항 연산자로 조건부 렌더링
-
 function App() {
-  const name = "리액트";
-  return (
-    <div>
-      {name === "리액트" ? (
-        <h1> 리액트입니다.</h1>
-      ) : (
-        <h2>리액트가 아닙니다.</h2>
-      )}
-    </div>
-  );
+  const name = "리웩트";
+  return <div>{name === "리액트" ? <h1> 리액트입니다.</h1> : null}</div>;
 }
 
 export default App;
