61e925449f85e5c5094636f6c334183c36223b9c
eddie553 <eddie553@naver.com>
2021-11-24 00:10:12 +0900

    3.4.2.3

--- hello-react/src/Say.js
+++ hello-react/src/Say.js
@@ -1,5 +1,6 @@
 // 3.4 state를 활용하여 Say 컴포넌트 만들기
 // 함수형 컴포넌트 - useState를 이용
+// useState 여러 번 사용하기
 
 import React, { useState } from 'react';
 
@@ -8,13 +9,23 @@ const Say = () => {
   const onClickEnter = () => setMessage('안녕하세요!');
   const onClickLeave = () => setMessage('안녕히 가세요!');
 
+  const [color, setColor] = useState('black');
+
   return (
     <div>
       <button onClick={onClickEnter}>입장</button>
       <button onClick={onClickLeave}>퇴장</button>
-      <h1>{message}</h1>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+        초록색
+      </button>
+      <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+        빨간색
+      </button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+        파란색
+      </button>
     </div>
   );
 };
-
 export default Say;
