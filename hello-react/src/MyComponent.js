2b669784612786bff69ca061ab821255e519ca12
eddie553 <eddie553@naver.com>
2021-11-23 23:54:16 +0900

    3.3.7-1

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,27 +1,29 @@
-// 3.3 isRequired로 필수 props 설정하기
+// 3.3 클래스형 컴포넌트에서 props 사용하기
 
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, favoriteNumber, children }) => {
-  return (
-    <div>
-      안녕하세요, 제 이름은 {name}입니다.
-      <br />
-      children 값은 {children}
-      입니다.
-      <br />
-      제가 좋아하는 숫자는 {favoriteNumber}입니다.
-    </div>
-  );
-};
+class MyComponent extends Component {
+  render() {
+    const { name, favoriteNumber, children } = this.props; //비구조화 할당
+    return (
+      <div>
+        안녕하세요, 제 이름은 {name}입니다.
+        <br />
+        children 값은 {children}
+        입니다.
+        <br />
+        제가 좋아하는 숫자는 {favoriteNumber}입니다.
+      </div>
+    );
+  }
+}
+
 MyComponent.defaultProps = {
   name: '기본이름',
 };
-
 MyComponent.propTypes = {
   name: PropTypes.string,
   favoriteNumber: PropTypes.number.isRequired,
 };
-
 export default MyComponent;
