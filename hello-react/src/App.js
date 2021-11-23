389cbd63a6ce53d810766d4d0604580436dd6ac2
eddie553 <eddie553@naver.com>
2021-11-23 23:51:20 +0900

    3.3.6-2

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,10 @@
 // 3.3 propTypes를 통한 props 검증
-// propTypes를 string으로 설정했으나 number로 전달하여 콘솔에 오류가 뜸
+// propTypes를 string으로 설정하고, string 타입인 props를 전달하여 오류 해결
 
 import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent name={3}>리액트</MyComponent>;
+  return <MyComponent name="react">리액트</MyComponent>;
 };
 export default App;
