d5490e1edbc0fe367831ae1b378945f84ac43f8d
eddie553 <eddie553@naver.com>
2021-11-23 21:32:52 +0900

    2.4.7

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,11 @@
-// 2.4.6 인라인 스타일링
-// 스타일을 적용할 때는 객체 형태로
-// 미리 선언하지 않고 바로 스타일 값 JSX 내부에서 전달하기
+// 2.4.7 class 대신 className
 
 import React from "react";
+import "./App.css";
 
 function App() {
   const name = "리액트";
-
-  return (
-    <div
-      style={{
-        backgroundColor: "black",
-        color: "aqua",
-        fontSize: "48px",
-        fontWeight: "italic",
-        padding: 16,
-      }}
-    >
-      {name}
-    </div>
-  );
+  return <div className="react">{name}</div>;
 }
 
 export default App;
