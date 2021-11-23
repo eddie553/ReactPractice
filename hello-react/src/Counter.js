e378ae72b29f6161d4af2259cc18c8c1975d76cd
eddie553 <eddie553@naver.com>
2021-11-24 00:05:15 +0900

    3.4.1.3

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -2,6 +2,7 @@
 // 클래스형 컴포넌트 - setState를 이용
 // constructor 메서드를 사용하지 않고 state의 초깃값 지정하기
 // this.setState를 두 번 사용해도 클릭 시 숫자는 1씩 더해진다.
+// -> 해결: this.setState의 인자로 함수를 넣어준다. (prevState)
 
 import React, { Component } from 'react';
 class Counter extends Component {
@@ -18,9 +19,16 @@ class Counter extends Component {
         <button
           // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
           onClick={() => {
-            // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
-            this.setState({ number: number + 1 });
-            this.setState({ number: this.state.number + 1 });
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+            // 위 코드와 아래 코드는 완전히 똑같은 기능을 합니다.
+            // 아래 코드는 함수에서 바로 객체를 반환한다는 의미입니다.
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
           }}
         >
           +1
