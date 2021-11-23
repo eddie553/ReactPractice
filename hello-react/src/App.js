3a7d78316a6b489546ccad75e4b770729c3a03ac
eddie553 <eddie553@naver.com>
2021-11-23 20:37:50 +0900

    2.4.1-2

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,15 @@
-import React from "react"
+import React from "react";
 
 // 2.4.1 감싸인 요소
-// 요소들을 하나의 부모 요소로 감싸주지 않아 오류가 발생하는 코드
+// div 요소로 감싸 오류를 해결한 코드
 
-function App() { 
+function App() {
   return (
- <h1>리액트 안녕!</h1> 
- <h2>잘 작동하니?</h2>
-  ) }
+    <div>
+      <h1>리액트 안녕!</h1>
+      <h2>잘 작동하니?</h2>{" "}
+    </div>
+  );
+}
+
+export default App;
