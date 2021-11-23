5ac81299147cddd4713595e6f19ce497507c0011
eddie553 <eddie553@naver.com>
2021-11-24 00:03:45 +0900

    3.4.1.3-1

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,9 +1,9 @@
 // 3.4 state를 활용하여 counter 컴포넌트 만들기
 // 클래스형 컴포넌트 - setState를 이용
 // constructor 메서드를 사용하지 않고 state의 초깃값 지정하기
+// this.setState를 두 번 사용해도 클릭 시 숫자는 1씩 더해진다.
 
 import React, { Component } from 'react';
-
 class Counter extends Component {
   state = {
     number: 0,
@@ -20,6 +20,7 @@ class Counter extends Component {
           onClick={() => {
             // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
             this.setState({ number: number + 1 });
+            this.setState({ number: this.state.number + 1 });
           }}
         >
           +1
