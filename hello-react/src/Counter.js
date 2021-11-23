5bc1a6b9f18c1216f3911ab44574b71576990355
eddie553 <eddie553@naver.com>
2021-11-24 00:01:04 +0900

    3.4.1.2

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,19 +1,16 @@
 // 3.4 state를 활용하여 counter 컴포넌트 만들기
 // 클래스형 컴포넌트 - setState를 이용
-// state 객체 안에 여러 값이 있을 때
+// constructor 메서드를 사용하지 않고 state의 초깃값 지정하기
 
 import React, { Component } from 'react';
+
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    // state의 초깃값 설정하기
-    this.state = {
-      number: 0,
-      fixedNumber: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
   render() {
-    const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회합니다. 여러 개의 값!
+    const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회합니다.
     return (
       <div>
         <h1>{number}</h1>
