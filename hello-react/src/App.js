c98fb7edbd61375ac1b525695e02801e7e1fa87d
eddie553 <eddie553@naver.com>
2021-11-23 21:41:01 +0900

    2.4.9

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,5 +1,4 @@
-// 2.4.8 태그는 꼭 닫아주기
-// 태그를 닫지 않아 생기는 오류 코드 해결하기
+// 2.4.9 JSX 주석 작성하는 방법
 
 import React from "react";
 import "./App.css";
@@ -8,9 +7,14 @@ function App() {
   const name = "리액트";
   return (
     <>
-      {" "}
-      <div className="react">{name}</div>
-      <input />{" "}
+      {/* 주석 작성하는 법 */}
+      <div
+        className="react" //시작 태그를 여러줄로 작성하게 된다면 여기에 주석 가능
+      >
+        {name}
+      </div>
+      // 하지만 이런 주석이나 /* 이런 주석은 페이지에 그대로 나타나게 된다. */
+      <input />
     </>
   );
 }
