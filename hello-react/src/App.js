509685376725a2e66eed5e4f34461674fdfb898f
eddie553 <eddie553@naver.com>
2021-11-23 20:38:30 +0900

    2.4.1-3

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 // 2.4.1 감싸인 요소
-// div 요소로 감싸 오류를 해결한 코드
+// Fragment 요소로 감싸 오류를 해결한 코드
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <h1>리액트 안녕!</h1>
       <h2>잘 작동하니?</h2>{" "}
-    </div>
+    </Fragment>
   );
 }
 
