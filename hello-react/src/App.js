9ad7fe93cba2dc11800875c25494e56dd87368f7
eddie553 <eddie553@naver.com>
2021-11-23 21:31:09 +0900

    2.4.6-2

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,19 +1,25 @@
 // 2.4.6 인라인 스타일링
 // 스타일을 적용할 때는 객체 형태로
+// 미리 선언하지 않고 바로 스타일 값 JSX 내부에서 전달하기
 
 import React from "react";
 
 function App() {
   const name = "리액트";
-  const style = {
-    backgroundColor: "black",
-    color: "aqua",
-    fontSize: "48px",
-    fontWeight: "italic",
-    padding: 16,
-  };
 
-  return <div style={style}>{name}</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "aqua",
+        fontSize: "48px",
+        fontWeight: "italic",
+        padding: 16,
+      }}
+    >
+      {name}
+    </div>
+  );
 }
 
 export default App;
