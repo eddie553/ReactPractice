f00163874872b73ce71c4d7b8795dc535f8482ab
eddie553 <eddie553@naver.com>
2021-11-23 21:26:30 +0900

    2.4.5-1

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,10 @@
-// 2.4.4 && 연산자를 사용한 조건부 렌더링
-// 조건을 만족하지 않을 때 아무것도 렌더링하지않음
+// 2.4.5 undefined 반환시 오류
 
 import React from "react";
 
 function App() {
-  const name = "리액트";
-  return <div>{name === "리액트" && <h1> 리액트입니다.</h1>}</div>;
+  const name = undefined;
+  return name;
 }
 
 export default App;
