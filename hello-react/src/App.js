3d2d2367d0c00cb2b30df142f179ca6c4f2576c0
eddie553 <eddie553@naver.com>
2021-11-23 23:50:01 +0900

    3.3.6-1

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,10 @@
-// 3.3 children
+// 3.3 propTypes를 통한 props 검증
+// propTypes를 string으로 설정했으나 number로 전달하여 콘솔에 오류가 뜸
 
 import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent>리액트</MyComponent>;
+  return <MyComponent name={3}>리액트</MyComponent>;
 };
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,6 +1,7 @@
-// 3.3 비구조화 할당 문법을 통해 props 내부 값 추출하기
+// 3.3 propTypes를 통한 props 검증
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const MyComponent = ({ name, children }) => {
   // 비구조화 할당
@@ -15,5 +16,8 @@ const MyComponent = ({ name, children }) => {
 };
 MyComponent.defaultProps = {
   name: '기본이름',
+};
+MyComponent.propTypes = {
+  name: PropTypes.string,
 };
 export default MyComponent;
