f6506f2c47f9f1d2c07699c0664677a407d1f3a6
eddie553 <eddie553@naver.com>
2021-11-23 23:47:13 +0900

    3.3.5-1

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,20 +1,19 @@
-// 3.3 children 받아오기
+// 3.3 비구조화 할당 문법을 통해 props 내부 값 추출하기
 
 import React from 'react';
 
 const MyComponent = (props) => {
+  const { name, children } = props; // 비구조화 할당
   return (
     <div>
-      안녕하세요, 제 이름은 {props.name}입니다.
+      안녕하세요, 제 이름은 {name}입니다.
       <br />
-      children 값은 {props.children}
+      children 값은 {children}
       입니다.
     </div>
   );
 };
-
 MyComponent.defaultProps = {
   name: '기본이름',
 };
-
 export default MyComponent;
