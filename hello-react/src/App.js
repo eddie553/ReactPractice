a29fd7b03e81c80f7ffe8c769aaafda06e2e10f2
eddie553 <eddie553@naver.com>
2021-11-23 23:37:47 +0900

    3.1-1

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,18 +1,11 @@
-// 2.5 ESLint , Prettier 도구
+// 3.1 함수형 컴포넌트
 
 import React from 'react';
 import './App.css';
 
 function App() {
   const name = '리액트';
-  return (
-    <div>
-      <div className="react">{name}</div>
-      <h1>들여쓰기가 이상한</h1>
-      <h2>코드</h2>
-      <p>입니다.</p>
-    </div>
-  );
+  return <div className="react">{name}</div>;
 }
 
 export default App;
