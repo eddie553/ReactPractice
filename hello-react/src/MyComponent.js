35865a438fc700f6c73170b55de480e04cc4da6e
eddie553 <eddie553@naver.com>
2021-11-23 23:56:46 +0900

    3.3.7-2

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,11 +1,19 @@
 // 3.3 클래스형 컴포넌트에서 props 사용하기
+// class 내부에서 defaultProps, propTypes 설정할 수도 있음
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
+  static defaultProps = {
+    name: '기본이름',
+  };
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
+  };
   render() {
-    const { name, favoriteNumber, children } = this.props; //비구조화 할당
+    const { name, favoriteNumber, children } = this.props;
     return (
       <div>
         안녕하세요, 제 이름은 {name}입니다.
@@ -18,12 +26,4 @@ class MyComponent extends Component {
     );
   }
 }
-
-MyComponent.defaultProps = {
-  name: '기본이름',
-};
-MyComponent.propTypes = {
-  name: PropTypes.string,
-  favoriteNumber: PropTypes.number.isRequired,
-};
 export default MyComponent;
